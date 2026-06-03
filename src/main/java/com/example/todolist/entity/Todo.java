8136b1c2dbeadc38dec5df8b3f8d56803a1c9883
marcosnasc-code <marcos.nacimento0321@gmail.com>
2026-06-02 21:52:15 -0300

    todolist init - first commit

---
+++ src/main/java/com/example/todolist/entity/Todo.java
@@ -0,0 +1,26 @@
+package com.example.todolist.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "todos")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private boolean realizado;
+    private int prioridade;
+
+}
